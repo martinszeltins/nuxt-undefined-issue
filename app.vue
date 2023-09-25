652f50b4b364f174220f247dee36fa343f448770
Martins <martins.zeltins@arkbauer.com>
2023-09-25 09:24:31 +0300

    [HEAD] Initial commit

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script setup lang="ts">
+    import countryCodes, { CountryProperty } from 'country-codes-list'
+
+    const countries = countryCodes.customList(
+        CountryProperty.countryCode, '{countryNameEn}'
+    )
+
+    console.log(countries)
+</script>
